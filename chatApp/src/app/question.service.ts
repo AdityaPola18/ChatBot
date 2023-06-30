da933c25d75dd3cf25b6ff6331f6be786fcc3c5f
AdityaPola18 <adityapola124@gmail.com>
2023-06-30 15:19:28 +0530

    second commit

---
+++ chatApp/src/app/question.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionService {
+
+  constructor() { }
+  questionsArray = [
+    {question: "From how long are you suffering?",
+     options: [{text:"1 day",isDisable: false},{text:"1 week",isDisable: false},{text:"1 month",isDisable: false}],
+     optionstype: "singleSelect",
+     answer: ""
+    },
+    {question: "How severe is it?",
+     options: [{text:"Mild",isDisable: false},{text:"Moderate",isDisable: false},{text:"High",isDisable: false}],
+     optionstype: "singleSelect",
+     answer: ""
+    },
+    {question: "Do you have any other symptom?",
+     options: [{text:"Yes",isDisable: false},{text:"No",isDisable: false}],
+     optionstype: "singleSelect",
+     answer: ""
+    }
+  ];
+  getQuestion(currentId: number){
+    if(currentId === this.questionsArray.length){
+      return null
+    }
+    else{
+      return this.questionsArray[currentId];
+
+    }
+  }
+  
+}
